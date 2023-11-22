f600b2b70973ea3dabb5049cb9be41abd7affaef
LaxmanZ <bhajantrilaxman2001@gmail.com>
2023-11-22 12:11:45 +0530

    Adding Other Components

---
+++ components/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div className="footer">
+      <p>@copy rights reserved -2023 & designed by Laxman Bhajantri</p>
+    </div>
+  );
+};
+
+export default Footer;
--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,15 @@ import Head from 'next/head';
 import Image from 'next/image';
 import { Inter } from 'next/font/google';
 import styles from '@/styles/Home.module.css';
+import Navbar from '@/components/Navbar';
+import Footer from '@/components/Footer';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
     <div>
+      <Navbar />
       <h1>HomePage</h1>
       <p>
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum,
@@ -21,6 +24,7 @@ export default function Home() {
         eligendi, corrupti fugiat, similique odit inventore fuga dolorem velit
         recusandae dolorum.
       </p>
+      <Footer />
     </div>
   );
 }
