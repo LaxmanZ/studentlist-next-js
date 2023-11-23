ae822041fc966186b2859433af65f610e7ec9d03
LaxmanZ <bhajantrilaxman2001@gmail.com>
2023-11-23 10:49:36 +0530

    fetching single item

--- pages/student/[id].js
+++ pages/student/[id].js
@@ -1,7 +1,38 @@
-const DetailsPage = () => {
+export const getStaticPaths = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const data = await res.json();
+
+  const paths = data.map((student) => {
+    return {
+      params: { id: student.id.toString() },
+    };
+  });
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps = async (context) => {
+  const id = context.params.id;
+  const res = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
+  const data = await res.json();
+
+  return {
+    props: {
+      student: data,
+    },
+  };
+};
+
+const DetailsPage = ({student}) => {
   return (
     <div>
-      <h1>Student Details Page</h1>
+      <h1>{student.name}</h1>
+      <p>{student.email}</p>
+      <p>{student.website}</p>
+      <p>{student.address.city}</p>
     </div>
   );
 };
